55e03fcdd475ad5caef7af492a0208b69e1d92dc
Chinedu <nedukwu@gmail.com>
2023-09-19 16:23:04 +0100

    85% done with the crypto news section

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,13 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
 // import { setupListeners } from "@reduxjs/toolkit/query/react";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(cryptoApi.middleware),
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(
+      cryptoApi.middleware,
+      cryptoNewsApi.middleware
+    );
+  },
 });
 
 // setupListeners(configureStore.dispatch);
