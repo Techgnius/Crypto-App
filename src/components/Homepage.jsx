96e3cf8133d23859d22892ce75a0fd262cd7a73b
Chinedu <nedukwu@gmail.com>
2023-09-22 13:52:15 +0100

    completed the cryptodetails page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -10,7 +10,7 @@ const { Title } = Typography;
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
-  console.log(data);
+  // console.log(data);
   if (isFetching) return "Loading...";
   return (
     <>
